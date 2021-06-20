47ec5b0deb2da9c2616fe5ceae5d764651ea619a
Brijesh Kumar <hello@dmi.systems>
2021-06-20 23:43:18 +0530

    TITLE CHANGED

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby Starter - Dimension V2',
-    author: 'Hunter Chang',
-    description: 'A Gatsby.js V2 Starter based on Dimension by HTML5 UP',
+    title: 'RKBL Family Trust',
+    author: 'RKBL_Trust',
+    description: 'Nurture and nourish nature & harness its bounty!',
   },
   plugins: [
     'gatsby-plugin-react-helmet',
--- src/components/Header.js
+++ src/components/Header.js
@@ -8,13 +8,13 @@ const Header = props => (
     </div>
     <div className="content">
       <div className="inner">
-        <h1>Dimension</h1>
+        <h1>RKBL FAMILY TRUST</h1>
         <p>
-          A fully responsive site template designed by{' '}
-          <a href="https://html5up.net">HTML5 UP</a> and released
+          Nurture and nourish nature & harness it's bounty!{' '}
+          <a href="https://rkbl-family-trust.netlify.app/">RKBL Family Trust</a> and released
           <br />
           for free under the{' '}
-          <a href="https://html5up.net/license">Creative Commons</a> license.
+          <a href="https://rkbl-family-trust.netlify.app/">Creative Commons</a> license.
         </p>
       </div>
     </div>
